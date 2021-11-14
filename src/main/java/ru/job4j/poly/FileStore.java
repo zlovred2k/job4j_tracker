5171e32f5495935e493f917d85b13e8cf6857091
Alex <zlovred2k@gmail.com>
2021-11-14 17:43:29 +0300

    Что такое полиморфизм

---
+++ src/main/java/ru/job4j/poly/FileStore.java
@@ -0,0 +1,13 @@
+package ru.job4j.poly;
+
+public class FileStore implements Store {
+    @Override
+    public void save(String value) {
+    }
+
+    @Override
+    public String[] load() {
+      return new String[] {};
+    }
+
+}
