adfb091ac4460120a2861438d12ae1129240051d
Alex <zlovred2k@gmail.com>
2021-11-18 01:23:49 +0300

    Интерфейс Input [#181092 #228961]

--- src/main/java/ru/job4j/poly/Bus.java
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -1,10 +1,20 @@
 package ru.job4j.poly;
 
-public class Bus implements Vehicle {
+public class Bus implements Transport {
 
     @Override
-    public void move() {
-        System.out.println(getClass().getSimpleName()
-        + " начал движение по трассе");
+    public void go() {
+        System.out.println("Автобус поехал");
+    }
+
+    @Override
+    public void passengers(int passengers) {
+        System.out.println("количество пассажиров" + passengers);
+    }
+
+    @Override
+    public double refuel(int fuel) {
+        return fuel * 54;
     }
 }
+
