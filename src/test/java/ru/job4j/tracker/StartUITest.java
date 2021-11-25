09114d9364aa7b63e362d71740d3519236831b6f
Alex <zlovred2k@gmail.com>
2021-11-25 20:41:19 +0300

    2.1. Тест на ValidateInput [#311887]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -198,4 +198,33 @@ public class StartUITest {
                         + "0. Exit Program" + ln
         ));
     }
+
+    @Test
+    public void whenInvalidInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(new String[] {"one", "1"});
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected, is(1));
+    }
+
+    @Test
+    public void whenInvalidManyInput() {
+        Output out = new StubOutput();
+        Input in = new StubInput(new String[] {"2", "1"});
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        int selected2 = input.askInt("Enter menu:");
+        assertThat(selected, is(2));
+        assertThat(selected2, is(1));
+    }
+
+    @Test
+    public void whenInvalidManyInputNegative() {
+        Output out = new StubOutput();
+        Input in = new StubInput(new String[] {"-2"});
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected, is(-2));
+    }
 }
