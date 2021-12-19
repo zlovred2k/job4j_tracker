15dfa176873cb55922a1fef29ba7904a468a6481
Alex <zlovred2k@gmail.com>
2021-12-19 20:08:12 +0300

    Подсчет функции в диапазоне. [#24251]

---
+++ src/main/java/ru/job4j/lambda/FuncDiapason.java
@@ -0,0 +1,16 @@
+package ru.job4j.lambda;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
+public class FuncDiapason {
+
+    public static List<Double> diapason(int start, int end, Function<Double, Double> func) {
+        List<Double> rsl = new ArrayList<>();
+        for (int i = start; i < end; i++) {
+            rsl.add(func.apply((double) i));
+        }
+        return rsl;
+    }
+}
---
+++ src/test/java/ru/job4j/lambda/FuncDiapasonTest.java
@@ -0,0 +1,33 @@
+package ru.job4j.lambda;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+public class FuncDiapasonTest {
+
+    @Test
+    public void whenLinearFunction() {
+        List<Double> result = FuncDiapason.diapason(5, 8, x -> 2 * x + 1);
+        List<Double> expected = Arrays.asList(11D, 13D, 15D);
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenSquarFunction() {
+        List<Double> result = FuncDiapason.diapason(5, 8, x -> x * x);
+        List<Double> expected = Arrays.asList(25D, 36D, 49D);
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenPowFunction() {
+        List<Double> result = FuncDiapason.diapason(5, 8, x -> Math.pow(2, x));
+        List<Double> expected = Arrays.asList(32D, 64D, 128D);
+        assertThat(result, is(expected));
+    }
+}
